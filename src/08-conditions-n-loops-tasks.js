aaecb0f77411ea8193f951f4dd4294a1da38e840
rysz-eriksson <ryszard.wolosiuk@gmail.com>
2020-05-13 16:39:23 +0200

    feat: new tesks

--- src/08-conditions-n-loops-tasks.js
+++ src/08-conditions-n-loops-tasks.js
@@ -235,8 +235,11 @@ function findFirstSingleChar(str) {
  *   5, 3, true, true   => '[3, 5]'
  *
  */
-function getIntervalString(/* a, b, isStartIncluded, isEndIncluded */) {
-  throw new Error('Not implemented');
+function getIntervalString(a, b, isStartIncluded, isEndIncluded) {
+  const start = isStartIncluded ? '[' : '(';
+  const end = isEndIncluded ? ']' : ')';
+  const middle = a < b ? `${a}, ${b}` : `${b}, ${a}`;
+  return `${start}${middle}${end}`;
 }
 
 
